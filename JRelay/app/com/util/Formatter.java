87c7666e127704503fad75920b33d06c0bc286c3
Robert Usey <ruusey@gmail.com>
2018-11-06 01:34:24 -0500

    Update to X31.0.3

--- JRelay/app/com/util/Formatter.java
+++ JRelay/app/com/util/Formatter.java
@@ -17,9 +17,10 @@ import com.app.JRelayGUI;
 @SuppressWarnings("unchecked")
 public class Formatter {
 	public static void main(String[] args) {
-//		String s = "6a39570cc9de4ec71d64821894c79332b197f92ba85ed281a023";
-//		System.out.println(s.substring(0,26));
-//		System.out.println(s.substring(26,s.length()));
+		String s = "6a39570cc9de4ec71d64821894c79332b197f92ba85ed281a023";
+		System.out.println(s.substring(0,26));
+		System.out.println(s.substring(26,s.length()));
+		return;
 //		
 //		try {
 //			String workingDir = JRelayGUI.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
@@ -28,7 +29,7 @@ public class Formatter {
 //			// TODO Auto-generated catch block
 //			e.printStackTrace();
 //		}
-		createPacketTypes();
+		//createPacketTypes();
 		//test();
 		
 		

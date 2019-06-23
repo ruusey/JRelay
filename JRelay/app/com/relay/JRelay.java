06d716f228574bbf06e39c3077a93591541835b2
Robert Usey <ruusey@gmail.com>
2019-06-23 03:53:40 -0400

    Latest

--- JRelay/app/com/relay/JRelay.java
+++ JRelay/app/com/relay/JRelay.java
@@ -48,7 +48,7 @@ public final class JRelay implements Runnable {
 	public static int NEXUS_GAMEID = -2;
 	public static int RANDOM_REALM_GAMEID = -3;
 	public static int MAPTEST_GAMEID = -6;
-	public static final String GAME_VERSION = "X31.7.2";
+	public static final String GAME_VERSION = "X31.7.3";
 	public static final String JRELAY_VERSION = "1.6.0";
 	
 	public static final boolean DEBUG = false;

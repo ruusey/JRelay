29e2e74182daddd96bc2be5e031048df8b8ab330
Robert Usey <ruusey@gmail.com>
2019-08-05 09:38:44 -0400

    JRelay for X31.8.3

--- JRelay/app/com/relay/JRelay.java
+++ JRelay/app/com/relay/JRelay.java
@@ -48,8 +48,8 @@ public final class JRelay implements Runnable {
 	public static int NEXUS_GAMEID = -2;
 	public static int RANDOM_REALM_GAMEID = -3;
 	public static int MAPTEST_GAMEID = -6;
-	public static final String GAME_VERSION = "X31.7.3";
-	public static final String JRELAY_VERSION = "1.6.0";
+	public static final String GAME_VERSION = "X31.8.3";
+	public static final String JRELAY_VERSION = "1.7.0";
 	
 	public static final boolean DEBUG = false;
 	public static final boolean PROD = true;
